84dc056cf33521c87d107c6cf9e2d4afa271eebc
embedded-audio24 <srzm@audioscan.com>
2025-11-06 14:27:17 -0500

    Restore robust audio playback buffer handling

--- Projects/STM32H743I-EVAL/Applications/USBX/Ux_Device_Audio2.0_PlayBack-test/USBX/App/ux_device_audio_play.h
+++ Projects/STM32H743I-EVAL/Applications/USBX/Ux_Device_Audio2.0_PlayBack-test/USBX/App/ux_device_audio_play.h
@@ -73,32 +73,6 @@ VOID usbx_audio_play_app_thread(ULONG arg);
 /* Size of audio buffer*/
 #define AUDIO_TOTAL_BUF_SIZE   192*600
 
-#define USBD_AUDIO_DEBUG_ENABLED        1U
-#define USBD_AUDIO_DEBUG_LOG_SIZE       128U
-
-typedef enum
-{
-  USBD_AUDIO_DEBUG_EVENT_STREAM_OPEN = 1U,     /* value0: alternate setting */
-  USBD_AUDIO_DEBUG_EVENT_STREAM_CLOSE,         /* value0: queued bytes, value1: alternate setting */
-  USBD_AUDIO_DEBUG_EVENT_BUFFER_RESET,         /* values unused */
-  USBD_AUDIO_DEBUG_EVENT_FRAME_RECEIVED,       /* value0: queued bytes before copy, value1: frame length */
-  USBD_AUDIO_DEBUG_EVENT_FRAME_DROPPED,        /* value0: queued bytes before drop, value1: frame length */
-  USBD_AUDIO_DEBUG_EVENT_RESERVE_WAIT,         /* value0: queued bytes when wait started, value1: requested bytes */
-  USBD_AUDIO_DEBUG_EVENT_RESERVE_GRANTED,      /* value0: queued bytes when granted, value1: reserved bytes */
-  USBD_AUDIO_DEBUG_EVENT_FRAME_COMMIT,         /* value0: queued bytes after commit, value1: committed bytes */
-  USBD_AUDIO_DEBUG_EVENT_PLAYBACK_ADVANCE,     /* value0: queued bytes after DMA advance, value1: consumed bytes */
-  USBD_AUDIO_DEBUG_EVENT_PLAYBACK_START,       /* value0: queued bytes, value1: stage (1=prime,2=queue,3=DMA) */
-  USBD_AUDIO_DEBUG_EVENT_SEMAPHORE_PUT         /* value0: queued bytes after release */
-} USBD_AUDIO_DebugEvent;
-
-typedef struct
-{
-  ULONG tick;
-  ULONG event;
-  ULONG value0;
-  ULONG value1;
-} USBD_AUDIO_DebugEntry;
-
 typedef enum
 {
   AUDIOPLAYER_STOP = 0,
@@ -136,10 +110,6 @@ typedef struct {
 /* USER CODE END PD */
 
 /* USER CODE BEGIN 2 */
-
-VOID USBD_AUDIO_DebugLogReset(VOID);
-VOID USBD_AUDIO_DebugLogSnapshot(USBD_AUDIO_DebugEntry *entries, ULONG max_entries, ULONG *out_count);
-const USBD_AUDIO_DebugEntry *USBD_AUDIO_DebugLogSnapshotGet(ULONG *out_count);
 
 /* USER CODE END 2 */
 
